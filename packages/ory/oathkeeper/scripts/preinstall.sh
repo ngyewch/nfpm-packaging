5fae6e16e58e261d87f8eb2205a2598f4edccea3
Nick Ng <ngyewch@gmail.com>
2025-07-22 12:44:44 +0800

    feat: create system user home directory

--- packages/ory/oathkeeper/scripts/preinstall.sh
+++ packages/ory/oathkeeper/scripts/preinstall.sh
@@ -9,7 +9,7 @@ case "$1" in
   install|upgrade|1|2)
     groupadd --system --force ${SERVICE_GROUP}
     SERVICE_GROUP_ID=$(getent group ${SERVICE_GROUP} | cut -d: -f3)
-    adduser --system --gid ${SERVICE_GROUP_ID} --no-create-home ${SERVICE_USER}
+    adduser --system --gid ${SERVICE_GROUP_ID} --home /var/lib/${SERVICE_USER} ${SERVICE_USER}
     mkdir -p /etc/ory/oathkeeper
   ;;
 
